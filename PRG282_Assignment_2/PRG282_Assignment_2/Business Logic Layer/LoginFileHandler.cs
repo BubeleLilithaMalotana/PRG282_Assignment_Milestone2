01c48c9ae9e59e19f42e61c3dc88273d0586106e
BubeleLilithaMalotana <92373041+BubeleLilithaMalotana@users.noreply.github.com>
2021-10-13 19:16:50 +0200

    Custom exceptions and updated filehandler

--- PRG282_Assignment_2/PRG282_Assignment_2/Business Logic Layer/LoginFileHandler.cs
+++ PRG282_Assignment_2/PRG282_Assignment_2/Business Logic Layer/LoginFileHandler.cs
@@ -13,34 +13,68 @@ namespace PRG282_Assignment_2.Business_Logic_Layer
 {
     class LoginFileHandler
     {
-        public LoginFileHandler()
-        {
+        public LoginFileHandler() { }
 
-        }
-        
         public void register(string username, string pass1, string pass2)
         {
-            if (username == null)
-            {
-                MessageBox.Show("Please enter a username");
-            }
-            else if (pass1 != pass2)
-            {
-                MessageBox.Show("The password do not match");
+            List<string> users = new List<string>();
+            List<string> pass = new List<string>();
 
-            }
-            else
+            try
             {
-
-                string myfile = @"LoginInfo.txt";
-
-                using (StreamWriter sw = File.AppendText(myfile))
+                if (username == string.Empty || pass1 == string.Empty || pass2 == string.Empty)
                 {
-                    sw.WriteLine(username + " " + pass1);
+                    throw new InvalidNameException("PLease ensure all options are filled in!");     // Using Exceptional Handling to ensure all text boxes are filled
                 }
-               
+                StreamReader sr = new StreamReader("LoginInfo.txt");
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] components = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    users.Add(components[0]);
+                    pass.Add(components[1]);
+
+                }
+
+                // If all text boxes are filled in correctly then it registers the user
+                if (pass1 == pass2 && username != string.Empty)
+                {
+                    if (!users.Contains(username))
+                    {
+
+                        string myfile = @"LoginInfo.txt";
+                        using (StreamWriter sw = File.AppendText(myfile))
+                        {
+                            sw.WriteLine(username + " " + pass1);
+                        }
+                        MangementMenu menu = new MangementMenu();
+                        menu.Show();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sorry, This username has been taken!");
+                    }
+                }
+
+                else
+                {
+                    throw new InvalidException("Password is not the same!");        // Using Exceptional Handling to inform the user that the Password and Confirm Password are not the same
+                }
+            }
+
+            catch (InvalidNameException ex1)
+            {
+                MessageBox.Show(ex1.Message);
+            }
+
+            catch (InvalidException ex2)
+            {
+                MessageBox.Show(ex2.Message);
             }
         }
+
+
 
         public void login(string username, string password)
         {
@@ -57,18 +91,28 @@ namespace PRG282_Assignment_2.Business_Logic_Layer
                 pass.Add(components[1]);
 
             }
+            try
+            {
+                if (users.Contains(username) && pass.Contains(password) && Array.IndexOf(users.ToArray(), username) == Array.IndexOf(pass.ToArray(), password))
+                {
+                    MangementMenu menu = new MangementMenu();
+                    menu.Show();
+                }
+                else
+                {
+                    throw new InvalidPasswordException("Incorrect Username or Password, Try again!");
+                }
 
-            if (users.Contains(username) && pass.Contains(password) && Array.IndexOf(users.ToArray(), username) == Array.IndexOf(pass.ToArray(), password))
-            {
-                MangementMenu menu = new MangementMenu();
-                menu.Show();
             }
-            else
+            catch (InvalidPasswordException ex1)
             {
-                MessageBox.Show("The Username and/or Password is incorect");
+                MessageBox.Show(ex1.Message);
             }
+
+
         }
 
-       
+
     }
 }
+
