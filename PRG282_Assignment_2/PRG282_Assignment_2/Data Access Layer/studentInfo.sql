bc98217d73758efff2b4c34c6be7d931a6fd248d
Brandon Frade <576307@student.belgiumcampus.ac.za>
2021-10-13 08:16:27 +0200

    added an sql database to the data access layer

---
+++ PRG282_Assignment_2/PRG282_Assignment_2/Data Access Layer/studentInfo.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE studentInformation;
+
+CREATE TABLE studentInformation (
+    StudentNumber INT,
+    StudentName VARCHAR(100),
+    Surname VARCHAR(100),
+	DateOfBirth Date,
+	Gender VARCHAR(100),
+	Phone VARCHAR(100),
+	StudentAddress VARCHAR(100)
+)
+
+CREATE TABLE moduleInformation(
+	ModuleCode VARCHAR(100),
+	ModuleName VARCHAR(100),
+	ModuleDescription VARCHAR(100),
+	Link VARCHAR(100)
+)
+
+SELECT * FROM studentInformation
+SELECT * FROM moduleInformation
